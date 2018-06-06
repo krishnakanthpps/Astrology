e4c1f94f1f5dd1bafe9ee820b53261b556b34b57
Harsha K <harsha@anagha.local>
2018-06-06 16:47:14 +0530

    forgotpasword java file

---
+++ app/src/main/java/dashboard/ForgotPasswordDialog.java
@@ -0,0 +1,13 @@
+package dashboard;
+
+import com.anagha.astrology.R;
+
+import utilitys.BaseActivity;
+
+public class ForgotPasswordDialog extends BaseActivity {
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.forgotpassword_dialog;
+    }
+}
